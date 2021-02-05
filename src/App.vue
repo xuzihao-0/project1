7211a5eadc2e8c6eeef9ce1b65b56fbe2807db4e
xuzh <1186004722@qq.com>
2021-02-05 23:26:04 +0800

    lunbotu wancheng

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,23 @@
 <template>
   <div id="app">
     <nav-bar></nav-bar>
+    <carousel></carousel>
+    <br><br><br><br><br><br><br><br><br>
     <router-view></router-view>
   </div>
 </template>
 <script>
 
+import Carousel from './components/common/Carousel '
 import NavBar from './components/common/NavBar'
+
 export default {
   name: "app",
   components:{
-    NavBar
+    NavBar,
+    Carousel
+
+
   }
 };
 </script>
--- src/components/common/Carousel .vue
+++ src/components/common/Carousel .vue
@@ -1,12 +1,61 @@
 <template>
+  <div>
+    <el-row :gutter="20">
+      <el-col :span="20" :offset="2"
+        ><div class="grid-content bg-purple">
+          <el-carousel :interval="5000" arrow="always" height="300px">
+            <el-carousel-item v-for="item in 4" :key="item">
 
+                <div class="img1">
+                  <img src="@/assets/img/1.jpg" alt="">
+                </div>
+
+
+            </el-carousel-item>
+          </el-carousel>
+        </div></el-col
+      >
+    </el-row>
+  </div>
 </template>
 <script>
 export default {
-  name:'carousel'
-}
+  name: "carousel",
+  data (){
+    return {
+      item: "hrlllo"
+    }
+  }
+};
 </script>
 <style scoped>
+.el-carousel__item h3 {
+  color: #475669;
+  font-size: 14px;
+  opacity: 0.75;
+  line-height: 200px;
+  margin: 0;
+}
+
+
+
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n + 1) {
+  background-color: #d3dce6;
+}
+.img1{
+  width:100%;
+  height: 100%;
+
+}
+img{
+  width:100%;
+
+
+}
 
 </style>
 
