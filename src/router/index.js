3c13838ea4eb3acdc3d5dacb48f32bd23c059bfd
xuzh <1186004722@qq.com>
2021-05-24 21:43:18 +0800

    this is a test

--- src/router/index.js
+++ src/router/index.js
@@ -1,3 +1,4 @@
+
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
@@ -13,7 +14,7 @@ const originalPush = VueRouter.prototype.push
 VueRouter.prototype.push = function push(location) {
   return originalPush.call(this, location).catch(err => err)
 }
-
+// fksdljfkjdsjflsdjlfja
 Vue.use(VueRouter)
 
 const routes = [
